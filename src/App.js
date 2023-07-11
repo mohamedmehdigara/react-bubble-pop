e281f20baeff6eaea126298d935fa99ff89c5e33
mohamedmehdigara <garamehdi57@yahoo.com>
2023-07-11 12:10:40 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const colors = ['red', 'green', 'blue', 'yellow'];
+
 function App() {
+  const [bubbles, setBubbles] = useState([]);
+
+  useEffect(() => {
+    // Generate initial bubbles
+    generateBubbles();
+  }, []);
+
+  const generateBubbles = () => {
+    const newBubbles = [];
+    for (let i = 0; i < 25; i++) {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      newBubbles.push({ id: i, color: randomColor });
+    }
+    setBubbles(newBubbles);
+  };
+
+  const handleClick = (bubbleId) => {
+    const updatedBubbles = bubbles.filter((bubble) => bubble.id !== bubbleId);
+    setBubbles(updatedBubbles);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bubble Pop</h1>
+      <div className="bubble-container">
+        {bubbles.map((bubble) => (
+          <div
+            key={bubble.id}
+            className={`bubble ${bubble.color}`}
+            onClick={() => handleClick(bubble.id)}
+          />
+        ))}
+      </div>
+      <button onClick={generateBubbles}>Reset</button>
     </div>
   );
 }
